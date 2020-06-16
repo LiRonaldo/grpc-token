79e2d802664d6bc3f2e4da088d27ed8f3082cfa0
yuxiangli8@126.com <yuxiangli8@126.com>
2020-06-16 19:26:25 +0800

    first commit

---
+++ hello/client/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+	pb "hello-protobuf-token/proto/hello" // 引入proto包
+)
+
+const (
+	// Address gRPC服务地址
+	Address = "localhost:50052"
+	OpenTLS = true
+)
+
+type customCredential struct {
+}
+
+func (c customCredential) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appid":  "101010",
+		"appkey": "i am key",
+	}, nil
+}
+
+// RequireTransportSecurity 自定义认证是否开启TLS
+func (c customCredential) RequireTransportSecurity() bool {
+	return OpenTLS
+}
+
+func main() {
+	var err error
+	var opts []grpc.DialOption
+
+	if OpenTLS {
+		// TLS连接
+		creds, err := credentials.NewClientTLSFromFile("C:/Users/admin/go/src/hello-protobuf-token/keys/server.pem", "WWW.BAIDU.COM")
+		if err != nil {
+			grpclog.Fatalf("Failed to create TLS credentials %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	// 使用自定义认证
+	opts = append(opts, grpc.WithPerRPCCredentials(new(customCredential)))
+
+	conn, err := grpc.Dial(Address, opts...)
+
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	// 初始化客户端
+	c := pb.NewHelloClient(conn)
+
+	// 调用方法
+	req := &pb.HelloRequest{Name: "gRPC"}
+	res, err := c.SayHello(context.Background(), req)
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+
+	println(res.Message)
+}
